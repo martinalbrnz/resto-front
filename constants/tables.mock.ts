eaa7ad4b457b1efacccc2ac9db3b6cf0895a81d9
Agustin Gomez <agusleongomez@gmail.com>
2023-01-02 20:43:15 -0300

    added tables mock

---
+++ constants/tables.mock.ts
@@ -0,0 +1,39 @@
+import { Table } from '../interfaces';
+
+export const tablesMock: Table[] = [
+  {
+    id: 1,
+    seats: 2,
+    posX: 50,
+    posY: 50,
+    shape: 'round',
+  },
+  {
+    id: 2,
+    seats: 2,
+    posX: 50,
+    posY: 200,
+    shape: 'round',
+  },
+  {
+    id: 3,
+    seats: 2,
+    posX: 50,
+    posY: 350,
+    shape: 'square',
+  },
+  {
+    id: 4,
+    seats: 2,
+    posX: 200,
+    posY: 50,
+    shape: 'square',
+  },
+  {
+    id: 5,
+    seats: 2,
+    posX: 200,
+    posY: 200,
+    shape: 'square',
+  },
+];
